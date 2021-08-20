ef8f60ebb88d7756077c53545c55294e2d7953ec
ushisha <usisha.s@gmail.com>
2021-08-20 09:46:23 +0100

    first commit

---
+++ src/components/Map.js
@@ -0,0 +1,38 @@
+import GoogleMapReact from 'google-map-react'
+import LocationMaker from './LocationMaker'
+
+const Map = ({ eventData, center, zoom }) => {
+  console.log(process.env)
+  const markers = eventData.map((ev) => {
+    if (ev.categories[0].id === 8) {
+      return (
+        <LocationMaker
+          key={ev.id}
+          lat={ev.geometries[0].coordinates[1]}
+          lng={ev.geometries[0].coordinates[0]}
+        />
+      )
+    }
+    return null
+  })
+  return (
+    <div className="map">
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_API_KEY }}
+        defaultCenter={center}
+        defaultZoom={zoom}
+      >
+        {markers}
+      </GoogleMapReact>
+    </div>
+  )
+}
+
+Map.defaultProps = {
+  center: {
+    lat: 34.052235,
+    lng: -118.243683,
+  },
+  zoom: 5,
+}
+export default Map
